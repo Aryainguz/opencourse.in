d30b9c8ada5eb4801c6ce8b09f2758bb897034df
Aryan Inguz <117257768+Aryainguz@users.noreply.github.com>
2025-05-19 19:06:44 +0530

    Update AdminConfig.jsx

--- configs/AdminConfig.jsx
+++ configs/AdminConfig.jsx
@@ -1,6 +1,7 @@
 export const adminConfig = {
   emails: ["aryans12345678@gmail.com",
     "gurpreetsehaj1005@gmail.com",
-    "pinak650.be22@chitkara.edu.in"
+    "pinak650.be22@chitkara.edu.in",
+           "karansharma.ab9904@gmail.com"
   ],
 };
